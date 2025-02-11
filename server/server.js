36deadcb86ab28c59ac29f94acee393dd1d31caa
Aditya Vardhan M S <111335503+aditya180604@users.noreply.github.com>
2025-02-11 12:13:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
