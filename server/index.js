cfef57b42d86f93364dd114e19c3370358e8eafc
Aditya Vardhan M S <111335503+aditya180604@users.noreply.github.com>
2025-02-11 12:13:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bcrypt = require('bcrypt');
 require("dotenv").config();
 const { v4: uuidv4 } = require("uuid");  // ✅ Import UUID
 
